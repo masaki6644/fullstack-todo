aa37cbbe50d685bf23a9fac4dc30d5a7d42089c1
masaki6644 <masaki6644@gmail.com>
2025-08-26 16:33:57 +0900

    frontend 完了

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     # 追加
     'rest_framework',
     'tasks',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    # テスト環境
+    "http://localhost:3000",
+]
